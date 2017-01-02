fe791ca1c74d38f1fa3e58b8651c636fffab37d3
Leo <pisceanfoot@gmail.com>
2017-01-02 19:53:54 +0800

    very basic

--- pselenium/selenium/webdriver.py
+++ pselenium/selenium/webdriver.py
@@ -5,16 +5,22 @@ from .common.driver import Driver
 from .import drivername
 
 import time
+import re
+import logging
+
+logger = logging.getLogger(__name__)
 
 class WebDriver(Driver):
 	def __init__(self, name, *arg, **kwarg):
 		super(WebDriver, self).__init__(name, *arg, **kwarg)
 
 	def implicitly_wait(self, second):
-		if(self.driver_name == drivername.CHROME):
-			print('chrome not support implicitly_wait, will use sleep instead')
-			time.sleep(second)
-		else:
-			super(WebDriver, self).implicitly_wait(second)
+		logger.debug('override implicitly_wait, sleep %s', second)
+		self.__wait(second)
 
+	def __wait(timeout):
+	    timeout = float(timeout)
+	    start = time()
+	    while time() - start < timeout:
+	        sleep(0.2)
 
